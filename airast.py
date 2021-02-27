2a1393b8b559044896ef679cba972e6ca67cd847
Lars Maier <lars.maier@tefax.net>
2021-02-27 22:46:37 +0100

    Fixed operator parsing.

---
+++ airast.py
@@ -0,0 +1,311 @@
+from enum import Enum
+
+
+class SourceLocation:
+    def __init__(self, begin, end):
+        self.begin = begin
+        self.end = end
+
+    def __add__(self, other):
+        if not isinstance(other, SourceLocation):
+            return NotImplemented
+        return SourceLocation(min(self.begin, other.begin), max(self.end, other.end))
+
+    def __str__(self):
+        return "{}-{}".format(self.begin, self.end)
+
+
+class AstNode:
+    def __init__(self, location=None):
+        self.value_type = AnyType()
+        self.location = location
+
+    def airify(self):
+        raise NotImplementedError
+
+    def is_int(self):
+        return isinstance(self.value_type, IntegerType)
+
+
+class BinaryOperator(Enum):
+    ADD = 0
+    SUB = 1
+    MUL = 2
+    DIV = 3
+    AND = 4
+    OR = 5
+    EQ = 6
+    NEQ = 7
+    LT = 8
+    LE = 9
+    GT = 10
+    GE = 11
+
+    def __str__(self):
+        return self.name()
+
+    def name(self):
+        strs = ["+", "-", "*", "/", "and", "or", "eq?", "ne?", "lt?", "le?", "gt?", "ge?"]
+        assert self.value < len(strs)
+        return strs[self.value]
+
+    def precedence(self):
+        precs = {
+            self.ADD: 0,
+            self.SUB: 0,
+            self.MUL: 1,
+            self.DIV: 1,
+            self.AND: -3,
+            self.OR: -3,
+            self.EQ: -2,
+            self.NEQ: -2,
+            self.LT: -2,
+            self.LE: -2,
+            self.GT: -2,
+            self.GE: -2,
+        }
+        assert self in precs
+        return precs[self]
+
+
+class BinaryExpression(AstNode):
+    def __init__(self, op, left, right):
+        super().__init__()
+        self.op = op
+        self.left = left
+        self.right = right
+
+    def airify(self):
+        return [self.op.name(), self.left.airify(), self.right.airify()]
+
+
+class UnaryOperator(Enum):
+    NEG = 0
+
+    def __str__(self):
+        if self.value == UnaryOperator.NEG:
+            return "neg"
+        else:
+            raise RuntimeError("Unknown unary operator")
+
+
+class UnaryExpression(AstNode):
+    def __init__(self, op, operand):
+        super().__init__()
+        self.op = op
+        self.operand = operand
+
+    def airify(self):
+        if self.op == UnaryOperator.NEG:
+            return ["-", 0, self.operand.airify()]
+        return [str(self.op), self.operand.airify()]
+
+
+class CaseExpression(AstNode):
+    def __init__(self, cases):
+        super().__init__()
+        self.cases = cases
+
+    def airify(self):
+        return ["if", *[[x[0].airify(), x[1].airify()] for x in self.cases]]
+
+
+class BlockExpression(AstNode):
+    def __init__(self, block):
+        super().__init__()
+        self.block = block
+
+    def airify(self):
+        if len(self.block) == 1:
+            return self.block[0].airify()
+        return ["seq", *[x.airify() for x in self.block]]
+
+
+class LetExpression(AstNode):
+    def __init__(self, bindings, block):
+        super().__init__()
+        self.bindings = bindings
+        self.block = block
+
+    def airify(self):
+        b = ([x[0].name, x[1].airify()] for x in self.bindings)
+        return ["let", list(b), *(x.airify() for x in self.block)]
+
+
+class CallExpression(AstNode):
+    def __init__(self, func, parameter):
+        super().__init__()
+        self.func = func
+        self.parameter = parameter
+
+        assert func is not None
+        assert all(p is not None for p in parameter)
+
+    def airify(self):
+        return [self.func.airify(), *[p.airify() for p in self.parameter]]
+
+
+class VariableReference(AstNode):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def airify(self):
+        return ["var-ref", self.name]
+
+
+class JsonValue(AstNode):
+    def __init__(self, value):
+        super().__init__()
+        self.value = value
+
+    def airify(self):
+        if isinstance(self.value, list):
+            return ["quote", self.value]
+        return self.value
+
+
+class IfThenElse(AstNode):
+    def __init__(self, cond, then, otherwise):
+        super().__init__()
+        self.cond = cond
+        self.then = then
+        self.otherwise = otherwise
+
+    def airify(self):
+        return ["if", [self.cond.airify(), self.then.airify()], [True, self.otherwise.airify()]]
+
+
+class Identifier(AstNode):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def airify(self):
+        return self.name
+
+
+class ForeachExpression(AstNode):
+    def __init__(self, var_binds, block):
+        super().__init__()
+        self.vars = var_binds
+        self.block = block
+
+    def airify(self):
+        return ["foreach", [[x[0].name, x[1].airify()] for x in self.vars], *[p.airify() for p in self.block]]
+
+
+class LambdaExpression(AstNode):
+    def __init__(self, params, captures, expr):
+        super().__init__()
+        self.params = params
+        self.captures = captures
+        self.expr = expr
+
+    def airify(self):
+        return ["lambda", ["quote", [x.name for x in self.captures]], ["quote", [x.name for x in self.params]],
+                ["quote", self.expr.airify()]]
+
+
+class TypeNode:
+    def is_compatible(self, other):
+        return super() == other
+
+
+class IntegerType:
+    def __eq__(self, other):
+        return isinstance(other, IntegerType)
+
+    def __str__(self):
+        return "int"
+
+
+class DoubleType:
+    def __eq__(self, other):
+        return isinstance(other, DoubleType)
+
+    def __str__(self):
+        return "double"
+
+    def is_compatible(self, other):
+        return isinstance(other, IntegerType) or isinstance(other, DoubleType)
+
+
+class BooleanType:
+    def __eq__(self, other):
+        return isinstance(other, BooleanType)
+
+    def __str__(self):
+        return "bool"
+
+
+class NullType:
+    def __eq__(self, other):
+        return isinstance(other, NullType)
+
+    def __str__(self):
+        return "null"
+
+
+class StringType:
+    def __eq__(self, other):
+        return isinstance(other, StringType)
+
+    def __str__(self):
+        return "string"
+
+
+class AnyType:
+    def __eq__(self, other):
+        return isinstance(other, AnyType)
+
+    def __str__(self):
+        return "any"
+
+    def is_compatible(self, other):
+        return True
+
+
+class ListType:
+    def __eq__(self, other):
+        if isinstance(other, ListType):
+            return self.base == other.base
+        return False
+
+    def __init__(self, base):
+        self.base = base
+
+    def __str__(self):
+        return "list<{}>".format(self.base)
+
+
+class DictType:
+    def __eq__(self, other):
+        if isinstance(other, DictType):
+            return self.base == other.base
+        return False
+
+    def __init__(self, base):
+        self.base = base
+
+    def __str__(self):
+        return "dict<{}>".format(self.base)
+
+
+class RecordType:
+    def __eq__(self, other):
+        if isinstance(other, RecordType):
+            if len(self.pairs) != len(other.pairs):
+                return False
+
+            for key, type_ in self.pairs.items():
+                if key not in other.pairs or other.pairs[key] != type_:
+                    return False
+            return True
+        return False
+
+    def __init__(self, pairs):
+        self.pairs = pairs
+
+    def __str__(self):
+        return "{" + ",".join(["{}: {}".format(name, type_) for (name, type_) in self.pairs]) + "}"
--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-import ast
+import airast
 
 
 class LookaheadIterator:
@@ -26,24 +26,41 @@ class LookaheadIterator:
         return self.lh
 
 
-class NumericValue:
-    def __init__(self, value):
+class ParserError(Exception):
+    def __init__(self, msg, location=None):
+        self.msg = msg
+        self.location = location
+
+    def __str__(self):
+        return "at {}: {}".format(self.location, self.msg)
+
+
+class Token:
+    def __init__(self, location=None):
+        self.location = location
+
+
+class NumericValue(Token):
+    def __init__(self, value, location=None):
+        super().__init__(location)
         self.value = value
 
     def __str__(self):
         return '{}f'.format(self.value)
 
 
-class StringValue:
-    def __init__(self, value):
+class StringValue(Token):
+    def __init__(self, value, location=None):
+        super().__init__(location)
         self.value = value
 
     def __str__(self):
         return '"{}"'.format(self.value)
 
 
-class Atom:
-    def __init__(self, name):
+class Atom(Token):
+    def __init__(self, name, location=None):
+        super().__init__(location)
         self.name = name
 
     def __eq__(self, other):
@@ -55,8 +72,9 @@ class Atom:
         return "`{}`".format(self.name)
 
 
-class Operator:
-    def __init__(self, op):
+class Operator(Token):
+    def __init__(self, op, location=None):
+        super().__init__(location)
         self.op = op
 
     def __eq__(self, other):
@@ -68,14 +86,43 @@ class Operator:
         return "{}".format(self.op)
 
 
+class Symbol(Token):
+    def __init__(self, sym, location=None):
+        super().__init__(location)
+        self.sym = sym
+
+    def __eq__(self, other):
+        if not isinstance(other, Symbol):
+            return NotImplemented
+        return self.sym == other.sym
+
+    def __str__(self):
+        return "{}".format(self.sym)
+
+
 def atomize(source):
     space = " \r\n\t\v"
     atom_terminator = "()[]{}\"#:;,`" + space
     special_chars = "=+-*/"
-    operators = "+-*/"
+    operators = "+-*/|><?!"
+    line = 1
+    line_start = 0
     i = 0
+
+    def get_position():
+        return line, i - line_start + 1
+
     while i < len(source):
+        start_offset = get_position()
+
+        def source_location():
+            return airast.SourceLocation(start_offset, get_position())
+
         if source[i] in space:
+            if source[i] == "\n":
+                line += 1
+                line_start = i + 1
+
             i += 1  # skip all whitespace
         elif source[i] == '#':
             # skip comment until end of line
@@ -88,9 +135,9 @@ def atomize(source):
                 value += source[i]
                 i += 1
                 if i >= len(source):
-                    raise RuntimeError("Unterminated string constant")
+                    raise ParserError("Unterminated string constant")
             i += 1
-            yield StringValue(value)
+            yield StringValue(value, source_location())
         elif source[i] == '`':
             value = ""
             i += 1
@@ -100,15 +147,15 @@ def atomize(source):
                 if i >= len(source):
                     raise RuntimeError("Unterminated quote sequence")
             i += 1
-            yield Atom(value)
+            yield Atom(value, source_location())
         elif source[i] in operators:
             c = source[i]
             i += 1
-            yield Operator(c)
+            yield Operator(c, source_location())
         elif source[i] in atom_terminator + special_chars:
             c = source[i]
             i += 1
-            yield c
+            yield Symbol(c, source_location())
         else:
             # read atom
             start = i
@@ -116,15 +163,15 @@ def atomize(source):
                 i += 1
             value = source[start:i]
             if value[0] in "0123456789":
-                yield NumericValue(float(value))
+                yield NumericValue(float(value), source_location())
             else:
-                yield Atom(value)
+                yield Atom(value, source_location())
 
 
 def expect_exact(it, expected):
     atom = it.next()
     if atom != expected:
-        raise RuntimeError("Unexpected {}, expected {}".format(atom, expected))
+        raise ParserError("Unexpected {}, expected {}".format(atom, expected), atom.location)
 
 
 def parse_block(it):
@@ -133,7 +180,7 @@ def parse_block(it):
     while it.lookahead() != Atom("end"):
         block.append(parse_block_expression(it))
     it.next()  # take end
-    return ast.BlockExpression(block)
+    return airast.BlockExpression(block)
 
 
 def parse_block_or_simple(it):
@@ -143,95 +190,91 @@ def parse_block_or_simple(it):
         return parse_simple_expression(it)
 
 
+def parse_block_or_complex(it):
+    if it.lookahead() == Atom("begin"):
+        return parse_block_expression(it)
+    else:
+        return parse_complex_expression(it)
+
+
 def parse_condition(it):
     cases = list()
     expect_exact(it, Atom("cond"))
-    expect_exact(it, ":")
+    expect_exact(it, Symbol(":"))
     while it.lookahead() == Atom("if"):
         it.next()  # take the if
-        cond = parse_block_or_simple(it)
+        cond = parse_complex_expression(it)
         expect_exact(it, Atom("do"))
-        expect_exact(it, ':')
-        then = parse_block_or_simple(it)
+        expect_exact(it, Symbol(':'))
+        then = parse_complex_expression(it)
         cases.append((cond, then))
     if it.lookahead() == Atom("otherwise"):
         it.next()
-        expect_exact(it, ":")
-        then = parse_block_or_simple(it)
-        cases.append((ast.JsonValue(True), then))
+        expect_exact(it, Symbol(':'))
+        then = parse_complex_expression(it)
+        cases.append((airast.JsonValue(True), then))
     expect_exact(it, Atom("end"))
-    return ast.CaseExpression(cases)
-
-
-def parse_simple_value(atom):
-    if isinstance(atom, StringValue):
-        return ast.JsonValue(atom.value)
-    elif isinstance(atom, NumericValue):
-        return ast.JsonValue(atom.value)
-    elif atom == Atom("true"):
-        return ast.JsonValue(True)
-    elif atom == Atom("false"):
-        return ast.JsonValue(False)
-    elif isinstance(atom, Atom):
-        if atom.name[0] == "$":
-            return ast.VariableReference(atom.name[1:])
-        else:
-            return ast.Identifier(atom.name)
+    return airast.CaseExpression(cases)
 
 
 def parse_lambda_expression(it):
+    expect_exact(it, Atom("lambda"))
     params = list()
     while True:
         var = parse_simple_value(it.next())
-        if not isinstance(var, ast.VariableReference):
-            raise RuntimeError("Expected variable name, found {}".format(var))
+        if not isinstance(var, airast.VariableReference):
+            raise RuntimeError("Expected variable name, found {}".format(var), var.location)
         params.append(var)
         if it.lookahead() in [Atom("using"), Atom("do")]:
             break
-        expect_exact(it, ',')
+        expect_exact(it, Symbol('', ''))
 
     captures = list()
     if it.lookahead() == Atom("using"):
         it.next()
         while True:
             var = parse_simple_value(it.next())
-            if not isinstance(var, ast.VariableReference):
+            if not isinstance(var, airast.VariableReference):
                 raise RuntimeError("Expected variable name, found {}".format(var))
             captures.append(var)
             if it.lookahead() == Atom("do"):
                 break
-            expect_exact(it, ',')
+            expect_exact(it, Symbol(','))
 
     expect_exact(it, Atom("do"))
-    expect_exact(it, ':')
+    expect_exact(it, Symbol(':'))
     block = list()
     while it.lookahead() != Atom("end"):
         block.append(parse_block_expression(it))
     it.next()  # take end
-    expr = ast.BlockExpression(block)
+    expr = airast.BlockExpression(block)
 
-    return ast.LambdaExpression(params, captures, expr)
+    return airast.LambdaExpression(params, captures, expr)
 
 
-def parse_simple_expression(it):
-    atom = it.next()
-    if atom == "(":
-        expr = parse_complex_expression(it)
-        expect_exact(it, ")")
-        return expr
-    elif atom == Atom("if"):
-        cond = parse_simple_expression(it)
-        expect_exact(it, ':')
-        then = parse_block_expression(it)
-        expect_exact(it, Atom("else"))
-        expect_exact(it, ':')
-        otherwise = parse_block_expression(it)
-        expect_exact(it, Atom("end"))
-        return ast.IfThenElse(cond, then, otherwise)
-    elif atom == Atom("lambda"):
-        return parse_lambda_expression(it)
-    else:
-        return parse_simple_value(atom)
+def parse_foreach_expression(it):
+    expect_exact(it, Atom("foreach"))
+    bindings = list()
+    while True:
+        var = parse_simple_value(it.next())
+        if not isinstance(var, airast.VariableReference):
+            raise RuntimeError("Expected variable reference, found {}".format(var))
+        expect_exact(it, Atom("in"))
+        val = parse_simple_expression(it)
+        bindings.append((var, val))
+        tok = it.lookahead()
+        if tok == Atom("do"):
+            it.next()
+            expect_exact(it, Symbol(':'))
+            break
+        expect_exact(it, Symbol(','))
+
+    body = list()
+    # consume more until the next end
+    while it.lookahead() != Atom("end"):
+        body.append(parse_block_expression(it))
+    it.next()  # take the end
+    return airast.ForeachExpression(bindings, body)
 
 
 def parse_let_expression(it):
@@ -239,29 +282,31 @@ def parse_let_expression(it):
     bindings = list()
     while True:
         var = parse_simple_value(it.next())
-        if not isinstance(var, ast.VariableReference):
+        if not isinstance(var, airast.VariableReference):
             raise RuntimeError("Expected variable reference, found {}".format(var))
-        expect_exact(it, "=")
-        val = parse_block_or_simple(it)
+        expect_exact(it, Symbol("="))
+        val = parse_block_or_complex(it)
         bindings.append((var, val))
         tok = it.lookahead()
-        if tok == ';':
+        if tok == Symbol(';'):
             it.next()
             break
-        expect_exact(it, ',')
+        expect_exact(it, Symbol(','))
 
     body = list()
     # consume more until the next end
     while it.lookahead() != Atom("end") and it.lookahead() is not None:
         body.append(parse_block_expression(it))
-    return ast.LetExpression(bindings, body)
+    return airast.LetExpression(bindings, body)
 
 
 def parse_function_call(it, func):
     def terminates_call(token):
-        if n in [')', ';', ':', ',']:
+        if token == Symbol('('):
+            return False
+        if isinstance(token, Symbol) or isinstance(token, Operator):
             return True
-        if n in [Atom("end"), Atom("else"), Atom("begin")]:
+        if n in [Atom("end"), Atom("else"), Atom("begin"), Atom("do")]:
             return True
 
         return False
@@ -272,80 +317,144 @@ def parse_function_call(it, func):
         if terminates_call(n):
             break
         parameter.append(parse_simple_expression(it))
-    return ast.CallExpression(func, parameter)
+    return airast.CallExpression(func, parameter)
 
 
-def parse_operator_application(it, left):
-    def product(first=None):
-        if first is None:
-            first = parse_simple_expression(it)
-        while True:
-            op = it.lookahead()
-            if op in [Operator('*'), Operator('/')]:
-                it.next()  # take operator
-                right = parse_simple_expression(it)
-                first = ast.BinaryExpression(
-                    ast.BinaryOperator.MUL if op == Operator('*') else ast.BinaryOperator.DIV,
-                    first, right
-                )
-            else:
-                break
-        return first
+def parse_operator(it):
+    op = it.next()
+    if op == Operator('+'):
+        return airast.BinaryOperator.ADD
+    elif op == Operator('-'):
+        return airast.BinaryOperator.SUB
+    elif op == Operator('*'):
+        return airast.BinaryOperator.MUL
+    elif op == Operator('/'):
+        return airast.BinaryOperator.DIV
 
-    def summation(first=None):
-        if first is None:
-            first = product()
-        while True:
-            op = it.lookahead()
-            if op in [Operator('+'), Operator('-')]:
-                it.next()  # take operator
-                right = product()
-                first = ast.BinaryExpression(
-                    ast.BinaryOperator.ADD if op == Operator('+') else ast.BinaryOperator.SUB,
-                    first, right
-                )
+    elif op == Atom("and"):
+        return airast.BinaryOperator.AND
+    elif op == Atom("or"):
+        return airast.BinaryOperator.OR
 
-            else:
-                break
-        return first
+    elif op == Operator("!"):
+        expect_exact(it, Symbol("="))
+        return airast.BinaryOperator.NEQ
+    elif op == Symbol("="):
+        expect_exact(it, Symbol("="))
+        return airast.BinaryOperator.EQ
 
-    return summation(product(left))
+    elif op == Operator('<'):
+        if it.lookahead() == Symbol('='):
+            it.next()
+            return airast.BinaryOperator.LE
+        return airast.BinaryOperator.LT
+    elif op == Operator('>'):
+        if it.lookahead() == Symbol('='):
+            it.next()
+            return airast.BinaryOperator.GE
+        return airast.BinaryOperator.GT
+
+    else:
+        raise ParserError("Unknown operator {}".format(op), op.location)
 
 
-def parse_complex_expression(it):
+def is_operator_ahead(it):
+    ahead = it.lookahead()
+    if isinstance(ahead, Operator):
+        return True
+    elif ahead in [Atom("and"), Atom("or"), Symbol("=")]:
+        return True
+    return False
+
+
+def parse_operator_expression(it, first):
+    operand_stack = [first]
+    operator_stack = []
+
+    def reduce_stacks():
+        assert len(operand_stack) >= 2
+        right = operand_stack.pop()
+        left = operand_stack.pop()
+        assert len(operator_stack) >= 1
+        next_op = operator_stack.pop()
+        operand_stack.append(airast.BinaryExpression(next_op, left, right))
+
+    while is_operator_ahead(it):
+        op = parse_operator(it)
+        while len(operator_stack) > 0 and operator_stack[-1].precedence() >= op.precedence():
+            reduce_stacks()
+        operator_stack.append(op)
+
+        opnd = parse_simple_expression(it)
+        operand_stack.append(opnd)
+
+    while len(operator_stack) != 0:
+        reduce_stacks()
+
+    assert len(operand_stack) == 1
+    return operand_stack[0]
+
+
+def parse_simple_value(atom):
+    if isinstance(atom, StringValue):
+        return airast.JsonValue(atom.value)
+    elif isinstance(atom, NumericValue):
+        return airast.JsonValue(atom.value)
+    elif atom == Atom("true"):
+        return airast.JsonValue(True)
+    elif atom == Atom("false"):
+        return airast.JsonValue(False)
+    elif isinstance(atom, Atom):
+        if atom.name[0] == "$":
+            return airast.VariableReference(atom.name[1:])
+        else:
+            return airast.Identifier(atom.name)
+
+
+def parse_simple_expression(it):
+    atom = it.next()
+    if atom == Symbol("("):
+        expr = parse_complex_expression(it)
+        expect_exact(it, Symbol(")"))
+        return expr
+    elif atom == Operator('-'):
+        expr = parse_simple_expression(it)
+        return airast.UnaryExpression(airast.UnaryOperator.NEG, expr)
+    else:
+        return parse_simple_value(atom)
+
+
+def parse_composed_expression(it):
     first = parse_simple_expression(it)
     ahead = it.lookahead()
     if isinstance(ahead, Operator):
-        return parse_operator_application(it, first)  # operator application
-    elif isinstance(ahead, Atom) or ahead == '(':
+        return parse_operator_expression(it, first)  # operator application
+    elif isinstance(ahead, Atom) or ahead == Symbol('('):
         return parse_function_call(it, first)
     else:
         return first
 
 
-def parse_foreach_expression(it):
-    expect_exact(it, Atom("foreach"))
-    bindings = list()
-    while True:
-        var = parse_simple_value(it.next())
-        if not isinstance(var, ast.VariableReference):
-            raise RuntimeError("Expected variable reference, found {}".format(var))
-        expect_exact(it, ":")
-        val = parse_simple_expression(it)
-        bindings.append((var, val))
-        tok = it.lookahead()
-        if tok == Atom("do"):
-            it.next()
-            expect_exact(it, ':')
-            break
-        expect_exact(it, ',')
+def parse_if_else_expression(it):
+    expect_exact(it, Atom("if"))
+    cond = parse_simple_expression(it)
+    expect_exact(it, Symbol(':'))
+    then = parse_block_expression(it)
+    expect_exact(it, Atom("else"))
+    expect_exact(it, Symbol(':'))
+    otherwise = parse_block_expression(it)
+    expect_exact(it, Atom("end"))
+    return airast.IfThenElse(cond, then, otherwise)
 
-    body = list()
-    # consume more until the next end
-    while it.lookahead() != Atom("end"):
-        body.append(parse_block_expression(it))
-    it.next()  # take the end
-    return ast.ForeachExpression(bindings, body)
+
+def parse_complex_expression(it):
+    ahead = it.lookahead()
+    if ahead == Atom("if"):
+        return parse_if_else_expression(it)
+    elif ahead == Atom("lambda"):
+        return parse_lambda_expression(it)
+    else:
+        return parse_composed_expression(it)
 
 
 def parse_block_expression(it):
@@ -359,7 +468,10 @@ def parse_block_expression(it):
     elif atom == Atom("foreach"):
         return parse_foreach_expression(it)
     else:
-        return parse_complex_expression(it)
+        expr = parse_complex_expression(it)
+        if it.lookahead() == Symbol(';'):
+            it.next()
+        return expr
 
 
 def parse(source):
@@ -367,4 +479,4 @@ def parse(source):
     it = LookaheadIterator(atomize(source))
     while it.lookahead() is not None:
         block.append(parse_block_expression(it))
-    return ast.BlockExpression(block)
+    return airast.BlockExpression(block)
